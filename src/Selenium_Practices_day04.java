a5f44402ecb3eb6ffb1d14a259c66bcf285f5f31
Panther69 Khoonkhum <panther69@Panther69s-Air.verizon.net>
2023-01-08 21:44:02 -0500

    Second Commit

--- src/Selenium_Practices_day04.java
+++ src/Selenium_Practices_day04.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+import java.util.Random;
+
+public class Selenium_Practices_day04 {
+        /**
+         * 1. Basic: Find sum of elements in an array
+         * 2. Basic: Print even and odd numbers from an array based on
+         * what user wants to print out
+         */
+        public static void main(String[] args) {
+            int[] numbers = new Random().ints(0, 20).limit(10).toArray();
+            System.out.println("Numbers generated: " + Arrays.toString(numbers));
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum = sum + numbers[i];
+            }
+
+            System.out.println(sum);
+            if(sum % 2 == 0){
+                System.out.println("This is even number");
+            }else{
+                System.out.println("This is odd number");
+            }
+        }
     }
-}
